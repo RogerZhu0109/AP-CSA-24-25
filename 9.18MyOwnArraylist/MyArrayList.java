ef7fe9cb16a3981fd9347fdfeb86d79de35b1dba
RogerZhu0109 <rez0109@icloud.com>
2024-09-18 12:47:48 -0400

    9.18

---
+++ 9.18MyOwnArraylist/MyArrayList.java
@@ -0,0 +1,46 @@
+public class MyArrayList {
+    private Object[] list;
+    private int logical_Size;
+    private int physical_Size;
+
+    public MyArrayList() {
+        list = new Object[10];
+        logical_Size = 0;
+
+    }
+
+    public void add() {
+
+    }
+
+    public void insert(int index) {
+
+    }
+
+    public Object get(int index) {
+
+        return -1;
+    }
+
+    public int getPhysicalSize() {
+        return physical_Size;
+    }
+
+    public int getLogicalSize() {
+        return logical_Size;
+    }
+
+    public int obtains(Object x) {
+
+        return -1;
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    private void increase_Size() {
+
+    }
+
+}
