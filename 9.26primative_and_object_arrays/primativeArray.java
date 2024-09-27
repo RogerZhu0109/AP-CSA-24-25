d284c91513233af36aebf097edc955e0c6817233
RogerZhu0109 <rez0109@icloud.com>
2024-09-27 17:29:59 -0400

    primitive

--- 9.26primative_and_object_arrays/primativeArray.java
+++ 9.26primative_and_object_arrays/primativeArray.java
@@ -1,3 +1,38 @@
-public class primativeArray {
+import java.util.*;
 
+public class primativeArray {
+    private int[] array;
+
+    public primativeArray(int size) {
+        array = new int[size];
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = rand.nextInt(100);
+        }
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void print() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            System.out.print(", ");
+        }
+        System.out.println("");
+    }
+
+    public void function1(int[] x) {
+        Random rand = new Random();
+        x[rand.nextInt(x.length)] = rand.nextInt(1000);
+        x[rand.nextInt(x.length)] = rand.nextInt(1000);
+    }
+
+    public void function2(int[] x) {
+        x = new int[5];
+        for (int i = 0; i < 5; i++) {
+            x[i] = i;
+        }
+    }
 }
--- 9.26primative_and_object_arrays/primativeTester.java
+++ 9.26primative_and_object_arrays/primativeTester.java
@@ -1,3 +1,13 @@
 public class primativeTester {
-
+    public static void main(String[] args) {
+        primativeArray list = new primativeArray(10);
+        System.out.println("Original");
+        list.print();
+        list.function1(list.getArray());
+        System.out.println("Modified");
+        list.print(); // does change becuase it points to the same location
+        System.out.println("Reallocating");
+        list.function2(list.getArray());
+        list.print(); // doest not change because it points to a new location
+    }
 }
