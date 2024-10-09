1e559a01691e09dab9650ec659530f61b106b267
RogerZhu0109 <rez0109@icloud.com>
2024-10-09 13:24:55 -0400

    update wenhao grade to be more accurate

--- 10.8parallelarrays/main.java
+++ 10.8parallelarrays/main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<student> array = new ArrayList<student>();
         array.add(new student("Roger", 100));
-        array.add(new student("Wenhao", 80));
+        array.add(new student("Wenhao", 60));
         System.out.println(array.get(0));
     }
 }
