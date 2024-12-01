9431e6b8878e5eedbf163865a8c83506ea927be3
RogerZhu0109 <rez0109@icloud.com>
2024-12-01 11:33:04 -0500

    buildingclass

--- buildingClass/Building.java
+++ buildingClass/Building.java
@@ -1,28 +1,23 @@
 package buildingClass;
 
 public abstract class Building implements Comparable<Building> {
-    int floors = 5;
+    private static int floors = 5;
 
-    public void setFloors(int floors) {
-        this.floors = floors;
+    public static void setFloors(int x) {
+        floors = x;
     }
 
-    public int getFLoors() {
+    public static int getFLoors() {
         return floors;
     }
 
     public abstract void calcCost(double x);
 
+    public abstract double getTotalCost();
+
     @Override
     public int compareTo(Building a) {
-
-        if (this.floors > a.floors) {
-            return 1;
-        } else if (this.floors < a.floors) {
-            return 0;
-        } else {
-            return 0;
-        }
+        return Double.compare(this.getTotalCost(), a.getTotalCost());
 
     }
 }
--- buildingClass/BuildingTester.java
+++ buildingClass/BuildingTester.java
@@ -2,6 +2,75 @@ package buildingClass;
 
 public class BuildingTester {
     public static void main(String[] args) {
+        Building b = new Apartment(15.5);
+        b.calcCost(12.5);
+        System.out.println(b.toString());
 
+        Building office = new OfficeSpace(20.0, 500.0);
+        office.calcCost(15.0);
+        System.out.println(office.toString());
+    }
+}
+
+class Apartment extends Building {
+    private double costPerFloor;
+    private double totalCost;
+
+    public Apartment(double costPerFloor) {
+        this.costPerFloor = costPerFloor;
+    }
+
+    public double getcostPerFloor() {
+        return costPerFloor;
+    }
+
+    public double gettotalCost() {
+        return totalCost;
+    }
+
+    public void calcCost(double x) {
+        totalCost = costPerFloor * getFLoors() + x;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    @Override
+    public String toString() {
+        return "Total Cost: " + totalCost;
+    }
+}
+
+class OfficeSpace extends Building {
+    private double costPerFloor;
+    private double totalCost;
+    private double squareFeetPerFloor;
+
+    public OfficeSpace(double costPerFloor, double squareFeetPerFloor) {
+        this.costPerFloor = costPerFloor;
+        this.squareFeetPerFloor = squareFeetPerFloor;
+    }
+
+    public double getcostPerFloor() {
+        return costPerFloor;
+    }
+
+    public double gettotalCost() {
+        return totalCost;
+    }
+
+    public void calcCost(double x) {
+        totalCost = ((costPerFloor * getFLoors()) + x) + (squareFeetPerFloor * 2.75);
+
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    @Override
+    public String toString() {
+        return "Total Cost: " + totalCost;
     }
 }
