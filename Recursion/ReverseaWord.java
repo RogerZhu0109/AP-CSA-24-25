1ee078cf6d688359afe5e861258311a363880ac4
RogerZhu0109 <rez0109@icloud.com>
2025-03-16 12:18:26 -0400

    reverseaword

---
+++ Recursion/ReverseaWord.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class ReverseaWord {
+    public static void main(String[] args) {
+        System.out.println(reverseString("Hot Coffee is better than iced coffee"));
+    }
+
+    public static String reverseString(String str) {
+        if (str.length() == 1) {
+            return str;
+        }
+        return str.charAt(str.length() - 1) + reverseString(str.substring(0, str.length() - 1));
+    }
+}
--- Recursion/makingChange.java
+++ Recursion/makingChange.java
@@ -2,13 +2,10 @@ package Recursion;
 
 public class makingChange {
     public static void main(String[] args) {
-        System.out.println(reverseString("Hot Coffee is better than iced coffee"));
+        System.out.println("Hi");
     }
 
-    public static String reverseString(String str) {
-        if (str.length() == 1) {
-            return str;
-        }
-        return str.charAt(str.length() - 1) + reverseString(str.substring(0, str.length() - 1));
+    public static String findCombinations(String str) {
+        return "";
     }
 }
