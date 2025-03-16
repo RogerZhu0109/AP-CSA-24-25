46fb68c33d87d9719543c027cea347d289e44f22
RogerZhu0109 <rez0109@icloud.com>
2025-03-16 10:51:13 -0400

    hi

--- Recursion/makingChange.java
+++ Recursion/makingChange.java
@@ -2,13 +2,13 @@ package Recursion;
 
 public class makingChange {
     public static void main(String[] args) {
-        System.out.println(reverseString("Hello World"));
+        System.out.println(reverseString("Hot Coffee is better than iced coffee"));
     }
 
     public static String reverseString(String str) {
         if (str.length() == 1) {
-            return str.substring(0, 1);
+            return str;
         }
-        return str.substring(str.length() - 1) + reverseString(str.substring(0, str.length() - 1));
+        return str.charAt(str.length() - 1) + reverseString(str.substring(0, str.length() - 1));
     }
 }
